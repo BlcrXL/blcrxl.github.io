219f15e19036718f5ffd5a362ce1b21cc0e089ae
TallBot <TallBot77@gmail.com>
2025-01-08 14:21:50 +0300

    Some pix for tossing

---
+++ IFPix/1.bat
@@ -0,0 +1,4 @@
+copy /b D:\Alex\Prvr\Pix\__\2\18082014130.jpg 
+copy /b D:\Alex\Prvr\Pix\__\2\OOigDDXY0qQвсаприто.jpg 
+copy /b "D:\Alex\Prvr\Pages\Фотофакт  белорусский сыродельный комбинат впервые выбрал свою королеву красоты. 21.by_files\Better\1395142592049145.jpg"
+copy /b "D:\Alex\Prvr\Pages\Фотофакты  самые красивые белорусские продавщицы и работницы райпо борются за приз в 100 млн рублей - Люди onliner_by_files\Better\e54eb10bfc2ec77ad1454514337e0f25.png" 
---
+++ IFPix/2.bat
@@ -0,0 +1,5 @@
+copy /b D:\Alex\Prvr\Pix\LastRing\xZud10A0u1o_0.jpg
+copy /b D:\Alex\Prvr\Pix\LastRing\O6ciS6ZQGFU_0.jpg
+copy /b D:\Alex\Prvr\Pix\LastRing\p30_zvonok3.jpg
+copy /b D:\Alex\Prvr\Pix\Dif\Untitled!.bmp
+copy /b "D:\Alex\Prvr\Pix\Фотографии Алены Кл?мко\Better\*"
